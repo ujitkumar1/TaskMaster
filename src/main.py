9386efdd1584d8856a26b241cf1d3b43c7329f36
Ujit <ujitkumar1@gmail.com>
2023-02-14 00:06:23 +0530

    Code updated in main.py

--- src/main.py
+++ src/main.py
@@ -25,6 +25,5 @@ api.add_resource(Todo, "/todo/<int:todo_id>")
 api.add_resource(TodoStatus, "/todo/status/<int:todo_id>")
 
 if __name__ == "__main__":
-    db.create_all()
     # running the flask app
     app.run(debug=True)
